ef424c2e47cd5c1d8a29f4279c3b804a29aef7d7
Mateo-Rey <jrey3darts@gmail.com>
2022-09-26 14:35:22 -0400

    sidebar plus music playing and front page

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Error = () => (
   <div className='w-full flex justify-center items-center'>
-    <h1 className='font-bold text-2xl text-white mt-2'>{title || 'Loading...'}</h1>
+    <h1 className='font-bold text-2xl text-white mt-2'>Something went wrong. Please try again</h1>
   </div>
 );
 
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -2,8 +2,11 @@ import { Error, Loader, SongCard } from "../components";
 import {genres} from '../assets/constants'
 import { useState } from "react";
 import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+import { useDispatch, useSelector } from "react-redux";
 
 const Discover = () => {
+    const dispatch = useDispatch()
+    const { activeSong, isPlaying } = useSelector((state) => state.player);
     const { data, isFetching, error } = useGetTopChartsQuery()
     const genreTitle = 'Pop'
 
@@ -39,6 +42,9 @@ const Discover = () => {
                         key={song.key}
                         song={song}
                         i={i}
+                        isPlaying={isPlaying}
+                        activeSong={activeSong}
+                        data={data}
                          />
                         )
                     })}
