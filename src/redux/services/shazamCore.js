e22aa2db4fe1471345ada249cdb8d6338d8a78da
Mateo-Rey <jrey3darts@gmail.com>
2022-09-27 10:12:25 -0400

    details header

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -16,10 +16,11 @@ export const shazamCoreApi = createApi({
   }),
   endpoints: (builder) => ({
     getTopCharts: builder.query({ query: () => '/charts/world'}),
-    getSongDetails: builder.query({ query: ({songid}) => `/tracks/details?track_id=${songid}` })
+    getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
   })
 });
 
 export const {
     useGetTopChartsQuery,
+    useGetSongDetailsQuery,
 } = shazamCoreApi
