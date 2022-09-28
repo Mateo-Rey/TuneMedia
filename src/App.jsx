6590ee0e2af0a07f24670c68576b2da66ee49746
Mateo-Rey <jrey3darts@gmail.com>
2022-09-27 22:01:36 -0400

    main

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,18 @@ import { Route, Routes } from 'react-router-dom';
 
 import { Searchbar, Sidebar, MusicPlayer, TopPlay } from './components';
 import { ArtistDetails, TopArtists, AroundYou, Discover, Search, SongDetails, TopCharts } from './pages';
-
+ 
 const App = () => {
   const { activeSong } = useSelector((state) => state.player);
 
   return (
     <div className="relative flex">
-      <Sidebar/>
-      
+      <Sidebar />
       <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
-      <div className='w-full'><Searchbar/></div>
-      
+        <Searchbar />
 
         <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
-        
           <div className="flex-1 h-fit pb-40">
-          
             <Routes>
               <Route path="/" element={<Discover />} />
               <Route path="/top-artists" element={<TopArtists />} />
@@ -34,13 +30,12 @@ const App = () => {
           </div>
         </div>
       </div>
-      
+
       {activeSong?.title && (
         <div className="absolute h-28 bottom-0 left-0 right-0 flex animate-slideup bg-gradient-to-br from-white/10 to-[#2a2a80] backdrop-blur-lg rounded-t-3xl z-10">
           <MusicPlayer />
         </div>
       )}
-      
     </div>
   );
 };
