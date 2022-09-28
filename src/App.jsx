c3c8cc3f02e5708b5f335c54dc2eda1bafd80f1b
Mateo-Rey <jrey3darts@gmail.com>
2022-09-27 21:34:59 -0400

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
       <Searchbar />
 
         <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
-        
+        <div className='md:hidden'><Searchbar/></div>
           <div className="flex-1 h-fit pb-40">
           
             <Routes>
