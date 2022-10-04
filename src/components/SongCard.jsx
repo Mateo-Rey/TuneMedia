c0027cc2dd57fbd0ccb9172508c40b339f0c5371
Mateo-Rey <jrey3darts@gmail.com>
2022-10-03 20:38:06 -0400

    fixed

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -21,8 +21,8 @@ const SongCard = ({ song, isPlaying, activeSong, data, i }) => {
   };
 
   return (
-    <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
-      <div className="relative w-full h-56 group">
+    <div className="flex flex-col w-[250px] justify-between p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
+      <div className="relative w-full h-56 justify-between group">
         <div className={`absolute inset-0 justify-around items-center bg-black bg-opacity-50 group-hover:flex ${activeSong?.title === song.title ? 'flex bg-black bg-opacity-70' : 'hidden'}`}>
           <PlayPause
             isPlaying={isPlaying}
@@ -38,7 +38,7 @@ const SongCard = ({ song, isPlaying, activeSong, data, i }) => {
         <img alt="song_img" src={song.images?.coverart} className="w-full h-full rounded-lg" />
       </div>
 
-      <div className="mt-4 flex flex-col">
+      <div className="mt-8 flex flex-col">
         <p className="font-semibold text-lg text-white truncate">
           <Link to={`/songs/${song?.key}`}>
             {song.title}
